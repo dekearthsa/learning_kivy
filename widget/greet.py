998ccea6301100b69a8d1b0e30684d6764f45327
pcsishun <pcsishun@gmail.com>
2024-09-23 12:05:16 +0700

    first commit

---
+++ widget/greet.py
@@ -0,0 +1,8 @@
+# from kivy.uix.widget import Widget
+
+# class WidgetGreet(Widget):
+#     def __init__(self, **kwargs):
+#         super(WidgetGreet, self).__init__(**kwargs)
+#          self.counting = 0
+    
+#     def greet(self):
